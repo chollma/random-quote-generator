639951057a7535d0cc08e12971771ffa0d3fc2b2
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-09 13:51:05 -0700

    Add quotes array
    Updated the quotes array in script.js with the information I had compiled earlier with outdoor-themed quotes.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,69 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
+// A container to hold all of the quotes to choose from
+  var quotes = [
+    {
+        quote: "The Antidote to exhaustion isn't rest. It's nature.",
+        source: 'Shikoba',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: 'Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.',
+        source: 'John Lubbock',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: 'He is richest who is content with the least, for content is the wealth of nature.',
+        source: 'Socrates',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: 'I took a walk in the woods and came out taller than the trees.',
+        source: 'Henry David Thoreau',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: 'The human spirit needs places where nature has not been rearranged by the hand of man.',
+        source: 'Proverb',
+        citation: '',
+        year: ''
+    },
+    { 
+        quote: 'All good things are wild and free.',
+        source: 'Proverb',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: 'In every walk with nature one receives far more than he seeks.',
+        source: 'John Muir',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.",
+        source: 'John Muir',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.",
+        source: 'Jack Kerouac',
+        citation: '',
+        year: ''
+    },
+    {
+        quote: "I go to nature every day for inspiration in the day's work.",
+        source: 'Frank Lloyd Wright',
+        citation: '',
+        year: ''
+    }
+  ]
 
 
 
@@ -30,4 +86,5 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 console.log('Test');
+console.log(quotes);
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
