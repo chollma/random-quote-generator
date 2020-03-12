059997d0e157ee9118be4fd90f2372aa32196f59
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-12 16:23:25 -0700

    Started printQuote function
    Utilizing document.write to print out random quote property on the page.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ project 1 - A Random Quote Generator
 // Selection of quotes to choose from: 
 var quotes = [
     { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '' },
-    { quote: 'Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.', source: 'John Lubbock', citation: '', year: '' },
-    { quote: 'He is richest who is content with the least, for content is the wealth of nature.', source: 'Socrates', citation: '', year: '' },
-    { quote: 'I took a walk in the woods and came out taller than the trees.', source: 'Henry David Thoreau', citation: '', year: '' },
-    { quote: 'The human spirit needs places where nature has not been rearranged by the hand of man.', source: 'Proverb', citation: '', year: '' },
-    { quote: 'All good things are wild and free.', source: 'Proverb', citation: '', year: '' },
-    { quote: 'In every walk with nature one receives far more than he seeks.', source: 'John Muir', citation: '', year: '' },
+    { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', citation: '', year: '' },
+    { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', citation: '', year: '' },
+    { quote: "I took a walk in the woods and came out taller than the trees.', source: 'Henry David Thoreau", citation: '', year: '' },
+    { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', citation: '', year: '' },
+    { quote: "All good things are wild and free.", source: 'Proverb', citation: '', year: '' },
+    { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: '', year: '' },
     { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: '', year: '' },
     { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', citation: '', year: '' },
     { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', citation: '', year: '' }
@@ -19,11 +19,14 @@ var quotes = [
 
 function getRandomQuote (array) {
   var number = Math.floor((Math.random() * quotes.length ) + 1 ); // Create a variable that generates a random number between zero and the last index in the quotes array 
-  var output = quotes[number]["quote"]; // Utilizing the number in the previous step as an index, access a quote from the object and assign it to the 'output' variable
-  return output;
+  var outputQuote = quotes[number]["quote"]; // Utilizing the number in the previous step as an index, access a quote from the object and assign it to the 'output' variable
+  return outputQuote;
   } 
 
-function printQuote (){}
-
+function printQuote (outputQuote,outputSource){}
+  var response = getRandomQuote(quotes);
+  var HTML = '<p class="quote">' + response + '</p>';
+  HTML += '<p class ="source"' + response + '</p>';
+  document.write(HTML);
 // Click Event Listener
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
