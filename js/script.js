ae06dce8c3c89583ce2dbe92a5656f776aaef5a9
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-13 14:22:55 -0700

    Completed if statements and print
    The program works!

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ var quotes = [
     { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', citation: '', year: '' },
     { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', citation: '', year: '' },
     { quote: "All good things are wild and free.", source: 'Proverb', citation: '', year: '' },
-    { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: '', year: '' },
-    { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: '', year: '' },
+    { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: 'Steep Trails', year: '1918' },
+    { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: 'The Unpublished Journals of John Muir', year: '1924' },
     { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', citation: '', year: '' },
     { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', citation: '', year: '' }
 ]
@@ -27,9 +27,20 @@ function printQuote (){
   var responseQuote = getRandomQuote(quotes); // Get a random quote object with properties
   var outputHTML = '<p class="quote">' + responseQuote.quote + '</p>';
   outputHTML += '<p class="source">' + responseQuote.source;
-   
+  
+  if (responseQuote.citation){
+    outputHTML += '<span class="citation">' + responseQuote.citation + '</span>';
+    
+  }
+  if (responseQuote.year) {
+    outputHTML += '<span class ="year">' + responseQuote.year + '</span>';
+  }
+
+  outputHTML += '</p>';
+  document.getElementById('quote-box').innerHTML = outputHTML; 
 }
 
   
 // Click Event Listener
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
