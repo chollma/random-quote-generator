33b8d7465eac3e1fef000ba9758da1181e6cf24a
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-13 14:35:33 -0700

    Added tags for outdoors theme
    Going for exceeds grade and this is a requirement!

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ Intended Behavior: A Random Quote Generator
 
 // Pre-selected a list of 10 outdoor themed quotes. 2 of them contain values for citation and year. 
 var quotes = [
-  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '' },
-  { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', citation: '', year: '' },
-  { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', citation: '', year: '' },
-  { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', citation: '', year: '' },
-  { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', citation: '', year: '' },
-  { quote: "All good things are wild and free.", source: 'Proverb', citation: '', year: '' },
-  { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: 'Steep Trails', year: '1918' },
-  { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: 'The Unpublished Journals of John Muir', year: '1924' },
-  { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', citation: '', year: '' },
-  { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', citation: '', year: '' }
+  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '', tags: 'outdoors'  },
+  { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', citation: '', year: '', tags: 'outdoors' },
+  { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', citation: '', year: '', tags: 'outdoors' },
+  { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', citation: '', year: '', tags: 'outdoors' },
+  { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', citation: '', year: '', tags: 'outdoors' },
+  { quote: "All good things are wild and free.", source: 'Proverb', citation: '', year: '', tags: 'outdoors' },
+  { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: 'Steep Trails', year: '1918', tags: 'outdoors' },
+  { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: 'The Unpublished Journals of John Muir', year: '1924', tags: 'outdoors' },
+  { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', citation: '', year: '', tags: 'outdoors' },
+  { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', citation: '', year: '', tags: 'outdoors' }
 ]
 // Generation of a random number to serve as the index for accessing the object above. Output should be a singular item object in the array.
 function getRandomQuote(array) {
