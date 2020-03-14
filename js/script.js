7ec2378be3bade07116c7e6da04dd5242d664b53
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-14 11:30:06 -0700

    Added auto refreshing
    Removed click listener and added refreshing every 10 seconds

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Intended Behavior: A Random Quote Generator
 
 // Pre-selected a list of 10 outdoor themed quotes. 2 of them contain values for citation and year. 
 var quotes = [
-  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '', tags: 'outdoors'  },
+  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '', tags: 'outdoors' },
   { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', citation: '', year: '', tags: 'outdoors' },
   { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', citation: '', year: '', tags: 'outdoors' },
   { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', citation: '', year: '', tags: 'outdoors' },
@@ -21,13 +21,13 @@ var quotes = [
 
 // Pre-selected list that contains the colors of the rainbow as strings of text
 var colors = [
-  { color: 'red'},
-  { color: 'orange'},
-  { color: 'yellow'},
-  { color: 'green'},
-  { color: 'indigo'},
-  { color: 'blue'},
-  { color: 'purple'}
+  { color: 'red' },
+  { color: 'orange' },
+  { color: 'yellow' },
+  { color: 'green' },
+  { color: 'indigo' },
+  { color: 'blue' },
+  { color: 'purple' }
 ]
 
 // Generation of a random number to serve as the index for accessing the object above. Output should be a singular item object in the array.
@@ -41,11 +41,11 @@ function getRandomColor(array) {
   var numberTwo = Math.floor((Math.random() * array.length) + 1);
   var outputColor = array[numberTwo];
   return outputColor;
-  
+
 }
 
 // Assembly of HTML output string utilizing the information returned from getRandomQuote
-function printQuote() {
+setInterval(function printQuote() {
   var responseQuote = getRandomQuote(quotes); // Get a random quote object with properties
   var responseColor = getRandomColor(colors).color; // Get random color object with properties
   var outputHTML = '<p class="quote">' + responseQuote.quote + '</p>';
@@ -64,10 +64,8 @@ function printQuote() {
   document.getElementById('tags').innerHTML = outputTags;
   document.getElementById('quote-box').innerHTML = outputHTML;
   document.body.style.backgroundColor = responseColor;
-}
+}, 10000);
 
 
 
-// Activation of the printQuote function everytime the user clicks the on-page button
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
 
