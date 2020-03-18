2fcf94c57c8c18bda11bef2cf3790687011907b5
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-03-18 12:41:45 -0700

    Removed unused properties

--- js/script.js
+++ js/script.js
@@ -7,16 +7,16 @@ Intended Behavior: A Random Quote Generator
 
 // Pre-selected a list of 10 outdoor themed quotes. 2 of them contain values for citation and year. 
 var quotes = [
-  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', citation: '', year: '', tags: 'outdoors' },
-  { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', citation: '', year: '', tags: 'outdoors' },
-  { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', citation: '', year: '', tags: 'outdoors' },
-  { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', citation: '', year: '', tags: 'outdoors' },
-  { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', citation: '', year: '', tags: 'outdoors' },
-  { quote: "All good things are wild and free.", source: 'Proverb', citation: '', year: '', tags: 'outdoors' },
+  { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba', tags: 'outdoors' },
+  { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock', tags: 'outdoors' },
+  { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates', tags: 'outdoors' },
+  { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau', tags: 'outdoors' },
+  { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb', tags: 'outdoors' },
+  { quote: "All good things are wild and free.", source: 'Proverb', tags: 'outdoors' },
   { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: 'Steep Trails', year: '1918', tags: 'outdoors' },
   { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: 'The Unpublished Journals of John Muir', year: '1924', tags: 'outdoors' },
-  { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', citation: '', year: '', tags: 'outdoors' },
-  { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', citation: '', year: '', tags: 'outdoors' }
+  { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac', tags: 'outdoors' },
+  { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright', tags: 'outdoors' }
 ]
 
 // Pre-selected list that contains the colors of the rainbow as strings of text
